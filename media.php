ab8edcf52acc57cbd1b4b395508fc96b8c8393b2
piconmtz <55458165+JuanPicon@users.noreply.github.com>
2022-10-02 04:51:57 -0500

    Update media.php

--- media.php
+++ media.php
@@ -2,10 +2,6 @@
 <channel/>
 <item title="Teletrak" link="https://mdstrm.com/live-stream-playlist/62616df888feb366839a3eb5.m3u8" thumbnail="https://www.m3u.cl/logo/376_Teletrak_Roku.png" description="CL | Deportes" category="Deportes"/>
 <item title="TV Turf Movil" link="https://janus.tvturf.cl/playlist/stream.m3u8" thumbnail="https://www.m3u.cl/logo/374_TV_Turf_Movil_Roku.png" description="CL | Deportes" category="Deportes"/>
-
 <item title="ESPN 1 MEX Full HD (1080p)" link="http://alfa.7057460.xyz:2095/live/Santillan2/Levaron/7275.ts" thumbnail="https://i.imgur.com/iy0EMz3.png" decription="MEX | Deportes" category="Deportes"/>
-
-<item title="ESPN 1 MEX Full HD (1080p)" link="http://nerdtv.xyz/cuenta/ssiptvList.php?user=Santillan2&password=Levaron&type=tv&category=.Deportes&format=.m3u" thumbnail="https://i.imgur.com/iy0EMz3.png" decription="MEX | Deportes" category="Deportes"/>
-
 <item title="Valparaiso Sporting" link="https://unlimited1-cl-isp.dps.live/sportinghd/sportinghd.smil/playlist.m3u8" thumbnail="https://www.m3u.cl/logo/375_Valparaiso_Sporting_Roku.png" description="CL | Deportes" category="Deportes"/>
 </rss>
