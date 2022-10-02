adbeda3bae699f5068a4682b20fc4735aa2961d2
piconmtz <55458165+JuanPicon@users.noreply.github.com>
2022-10-02 04:58:37 -0500

    Delete media.php

--- media.php
+++
@@ -1,6 +0,0 @@
-<rss xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
-<channel/>
-<item title="Teletrak" link="https://mdstrm.com/live-stream-playlist/62616df888feb366839a3eb5.m3u8" thumbnail="https://www.m3u.cl/logo/376_Teletrak_Roku.png" description="CL | Deportes" category="Deportes"/>
-<item title="TV Turf Movil" link="https://janus.tvturf.cl/playlist/stream.m3u8" thumbnail="https://www.m3u.cl/logo/374_TV_Turf_Movil_Roku.png" description="CL | Deportes" category="Deportes"/>
-<item title="Valparaiso Sporting" link="https://unlimited1-cl-isp.dps.live/sportinghd/sportinghd.smil/playlist.m3u8" thumbnail="https://www.m3u.cl/logo/375_Valparaiso_Sporting_Roku.png" description="CL | Deportes" category="Deportes"/>
-</rss>
